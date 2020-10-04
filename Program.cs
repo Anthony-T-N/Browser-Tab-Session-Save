77fa9d95f91cc68e77f9eb21eb9a53c3f19104a5
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-10-04 13:29:16 +1030

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+/* Method
+ * 1) Select opened web browser.  
+ * 2) Create bookmark folder with today's date as name.
+ * 3) Individually add tab sessions to the folder.
+ * 4) Close tab once added (Ctrl + w)
+ */
+
+namespace Browser_Tab_Session_Save
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://github.com/TestStack/White
+            // https://docs.microsoft.com/en-us/dotnet/framework/ui-automation/ui-automation-overview?redirectedfrom=MSDN
+
+        }
+    }
+}
