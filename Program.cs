5060f5bf334191533683aad048d6795e90e8ebfd
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-10-05 12:50:09 +1030

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-using System;
-
 /* Method
  * 1) Select opened web browser.  
  * 2) Create bookmark folder with today's date as name.
@@ -7,15 +5,38 @@ using System;
  * 4) Close tab once added (Ctrl + w)
  */
 
+// https://github.com/TestStack/White
+// https://docs.microsoft.com/en-us/dotnet/framework/ui-automation/ui-automation-overview?redirectedfrom=MSDN
+// https://archive.codeplex.com/?p=inputsimulator
+
+using WindowsInput;
+using System;
+
+
 namespace Browser_Tab_Session_Save
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // https://github.com/TestStack/White
-            // https://docs.microsoft.com/en-us/dotnet/framework/ui-automation/ui-automation-overview?redirectedfrom=MSDN
-
+            Program main_program = new Program();
+            main_program.test();
         }
+
+        public void test()
+        {
+            var sim = new InputSimulator();
+            sim.Mouse.MoveMouseBy(5, 34);
+            System.Threading.Thread.Sleep(1000);
+            sim.Mouse.MoveMouseBy(10, 34);
+            System.Threading.Thread.Sleep(1000);
+            sim.Mouse.MoveMouseBy(50, 34);
+        }
+        public void create_bookmark_folder()
+        {
+            string folder_name = DateTime.Now.ToString("yyyy-MM-dd h:mm:ss tt");
+            
+        }
+
     }
 }
